a56761d80c604ff1900c59ce54a3a76b90a7bccc
Mina Atef <mina.atef0@gmail.com>
2021-09-24 19:17:40 +0200

    install script

---
+++ install.sh
@@ -0,0 +1,22 @@
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+git clone https://github.com/minaaaatef/blink
+cd blink
+cd BackEnd
+echo "${green}>>> Creating virtualenv${reset}"
+virtualenv venv
+sleep 2
+echo "${green}>>> activate the venv.${reset}"
+source venv/bin/activate
+echo "${green}>>> Installing the requirements${reset}"
+pip install -r requirements.txt 
+echo "${green}>>> Installing honcho to run ProcFile${reset}"
+pip install honcho
+echo "${green}>>> Installing npm packages${reset}"
+cd ../FrontEnd/vuetify-basic-components-master
+npm install
+echo "${green}>>> Running ProcFile ${reset}"
+cd ../../
+pwd
+honcho start
