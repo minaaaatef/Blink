418bc87e9249dc7bf9a6c385e251ffb1c03a390d
Mina Atef <mina.atef0@gmail.com>
2021-09-25 13:47:09 +0200

    urls Update

--- BackEnd/BLink/urls.py
+++ BackEnd/BLink/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls import url
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Snippets API",
+        title="Loan API",
         default_version='v1',
         description="Test description",
         terms_of_service="https://www.google.com/policies/terms/",
@@ -21,7 +21,7 @@ schema_view = get_schema_view(
 urlpatterns = [
 
     url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
     path('admin/', admin.site.urls),
